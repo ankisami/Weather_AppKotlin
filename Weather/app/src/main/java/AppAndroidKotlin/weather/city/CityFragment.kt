7e94f94dd3e9146f10ea940f77400dc8097d8280
ankisami <sami.anki1316@gmail.com>
2020-04-03 16:05:31 +0200

    Affichage de la ville enregistré dans la bdd

--- Weather/app/src/main/java/AppAndroidKotlin/weather/city/CityFragment.kt
+++ Weather/app/src/main/java/AppAndroidKotlin/weather/city/CityFragment.kt
@@ -8,11 +8,15 @@ import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class CityFragment: Fragment() {
+class CityFragment: Fragment(), CityAdapter.CityItemListener {
 
     private lateinit var cities: MutableList<City>
     private lateinit var  database: Database
+    private lateinit var recyclerView: RecyclerView
+    private  lateinit var adapter: CityAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +26,29 @@ class CityFragment: Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val view = inflater?.inflate(R.layout.fragment_city, container, false)
+        savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_city, container, false)
+        recyclerView = view.findViewById(R.id.cities_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(context)
         return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = CityAdapter(cities, this)
+        recyclerView.adapter = adapter
+    }
+
+
+    override fun onCitySelected(city: City) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCityDeleted(city: City) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -68,5 +91,7 @@ class CityFragment: Fragment() {
                Toast.LENGTH_SHORT).show()
        }
     }
+
+
 
 }
